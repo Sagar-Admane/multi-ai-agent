611bf0b8e0b399e89f8b047226412924e080d2e7
Sagar Admane <sagaradmane6@gmail.com>
2025-12-05 19:00:15 +0530

    changed package.json and added routing

---
+++ src/routing/intentRoute.ts
@@ -0,0 +1,8 @@
+import express from "express"
+import { intentOfText } from "../controller/intentOfText.js";
+
+const route = express.Router();
+
+route.post("/", intentOfText);
+
+export default route;
