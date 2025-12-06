5df450cd0fa2bbd313d47dd21bce9821c432c830
Sagar Admane <sagaradmane6@gmail.com>
2025-12-07 03:25:04 +0530

    controller

---
+++ src/controller/memoryController.ts
@@ -0,0 +1,62 @@
+import { text } from "stream/consumers";
+import { mcp } from "../client.js";
+
+export async function saveMemory(req : any, res : any){
+    try {
+        const tool = req.body.intent;
+        const response = await mcp.callTool({
+            name : `${tool}`,
+            arguments : {
+                text : req.body.text
+            }
+        })
+
+        if(!response){
+            return res.json({
+                message : "Unable to save the memory"
+            })
+        }
+
+        console.log(response)
+
+        return res.json({
+            text : req.body.text,
+            intent : req.body.intent,
+            message : "Saved in memory successfully"
+        })
+    } catch (error) {
+        res.error({
+            error : `Error while saving the memory : ${error}`
+        })
+    }
+}
+
+export async function queryMemory(req : any, res : any){
+    try {
+        const tool = req.body.intent;
+        const response = await mcp.callTool({
+            name : `${tool}`,
+            arguments : {
+                text : req.body.text
+            }
+        })
+
+        if(!response){
+            return res.json({
+                message : "Unable to query the memory"
+            })
+        }
+
+        console.log(response.content)
+
+        return res.json({
+            text : req.body.text,
+            intent : req.body.intent,
+            message : `${response.content[0].text}`
+        })
+    } catch (error) {
+        res.error({
+            error : `Error while saving the memory : ${error}`
+        })
+    }
+}
