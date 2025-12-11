728c42ccde734b93224f72e8fb1c0cc792c4cf90
Sagar Admane <sagaradmane6@gmail.com>
2025-12-11 23:50:56 +0530

    Added a new routing handler

--- src/client.ts
+++ src/client.ts
@@ -2,6 +2,7 @@ import { Client } from "@modelcontextprotocol/sdk/client"
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js"
 import express from "express"
 import route from "../src/routing/memoryRoutes.js"
+import taskRoute from "../src/routing/taskRoute.js"
 
 const clientApp = express()
 clientApp.use(express.json())
@@ -16,6 +17,7 @@ export const mcp = new Client({
 })
 
 clientApp.use("/memory", route)
+clientApp.use("/", taskRoute);
 
 const transport = new StdioClientTransport({
     command : "npm",
