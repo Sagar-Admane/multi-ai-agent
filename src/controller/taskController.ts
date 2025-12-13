988dff05b5e2e4d5318a14b7fc48d8c3454ab67d
Sagar Admane <sagaradmane6@gmail.com>
2025-12-13 18:08:13 +0530

    task controller changed

--- src/controller/taskController.ts
+++ src/controller/taskController.ts
@@ -6,7 +6,8 @@ export async function getTask(req : any, res : any){
         const result = await getTasks(req.body.text);
         console.log(result);
         return res.json({
-            result
+            result,
+            text : req.body.text
         })
     } catch (error) {
         console.log(error);
