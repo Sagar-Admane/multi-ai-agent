ae54eb5e3f129e9a4469a3d552d10bafc1050042
Sagar Admane <sagaradmane6@gmail.com>
2025-12-22 23:08:10 +0530

    Added the utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,14 @@
+import Expense from "../models/expenseModel.js"
+import { Document } from "mongoose";
+
+interface ExpenseDoc extends Document{
+    type: string;
+    amount: number;
+    date?: NativeDate | null | undefined;
+    merchant?: string | null | undefined;
+    bankName?: string | null | undefined;
+}
+
 export default function cleanJSON(str : string | any) {
     return str
         .replace(/```json/gi, "")
@@ -5,4 +16,15 @@ export default function cleanJSON(str : string | any) {
         .replace(/\n/g, "")
         .replace(/\r/g, "")
         .trim();
+}
+
+export function convertToText(expenses : ExpenseDoc[]){
+
+    const result = expenses.map((m) => {
+        const amount = m.amount;
+        const type = m.type;
+        const bankName = m.bankName
+        return `The amount was ${type} by ${amount} in the bank :- ${bankName}`;
+    })
+    return result;
 }
